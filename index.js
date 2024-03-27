eb5a5bd75a59237e5a7ee243ae9549f745ca1073
Yin <doyin@deloitte.com.cn>
2024-03-27 17:45:49 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,9 @@
+const generateUUID = require('./generateUUID')
+const getArraySum = require('./getArraySum')
+const isEvenNumber = require('./isEvenNumber')
+const getCurrentTimestamp = require('./getCurrentTimestamp')
+const print = require('./print')
+
+module.exports = (str) => {
+    return str.toUpperCase();
+  }
